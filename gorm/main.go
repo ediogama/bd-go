5c7bfacc1784eba53a131158b9d2d833efe875c1
Edio Gama <ediogama@hotmail.com>
2023-01-16 23:15:57 -0300

    add belongs to

--- gorm/main.go
+++ gorm/main.go
@@ -7,62 +7,41 @@ import (
 	"gorm.io/gorm"
 )
 
+type Category struct {
+	ID   int `gorm:"primaryKey"`
+	Name string
+}
+
 type Product struct {
-	ID    int `gorm:"primaryKey"`
-	Name  string
-	Price float64
+	ID         int `gorm:"primaryKey"`
+	Name       string
+	Price      float64
+	CategoryId int
+	Category   Category
+	gorm.Model
 }
 
 func main() {
-	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
-	db.AutoMigrate(&Product{})
+	db.AutoMigrate(&Product{}, &Category{})
 
-	// create
-	// db.Create(&Product{
-	// 	Name: "Notebook",
-	// 	Price: 1000.00,
-	// })
+	// categoryEletro := Category{Name: "Eletronicos"}
+	// db.Create(&categoryEletro)
 
-	// create batch
-	// ps := []Product{
-	// 	{Name: "Notebook", Price: 1000.00},
-	// 	{Name: "Mouse", Price: 50.00},
-	// 	{Name: "Keyboard", Price: 100.00},
-	// }
-	// db.Create(&ps)
+	db.Create(&Product{
+		Name:       "Mouse",
+		Price:      50.00,
+		CategoryId: 1,
+	})
 
-	// select one
-	// var product Product
-	// db.First(&product, 3)
-	// fmt.Println(product)
-	// db.First(&product, "name = ?", "Mouse")
-	// fmt.Println(product)
-
-	//select all
-	//var products []Product
-	// db.Limit(2).Offset(2).Find(&products)
-	// for _, p := range products {
-	// 	fmt.Println(p)
-	// }
-
-	//where
-	// db.Where("name LIKE ?", "%k%").Find(&products)
-	// for _, p := range products {
-	// 	fmt.Println(p)
-	// }
-
-	var p Product
-	db.First(&p, 1)
-	p.Name = "New Mouse"
-	db.Save(&p)
-
-	var p2 Product
-	db.First(&p2, 1)
-	fmt.Println(p2.Name)
-	db.Delete(&p2)
+	var products []Product
+	db.Preload("Category").Find(&products)
+	for _, p := range products {
+		fmt.Println(p.Name, p.Category.Name)
+	}
 
 }
